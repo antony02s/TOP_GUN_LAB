80811e3722c7b748f51c25c1b565580da130fff1
Antony Sánchez Herrera <77695208+antony02s@users.noreply.github.com>
2023-06-07 12:33:30 -0500

    Functions and data types

---
+++ WORKSHOP_1/Functions and data types/excercise_2/reverse_string.py
@@ -0,0 +1,9 @@
+# Create a Python function called
+# reverse_string that takes a string as input and returns
+# the reversed string.
+
+def reverse_string(string):
+    new_str = string[::-1]
+
+    return new_str
+    
