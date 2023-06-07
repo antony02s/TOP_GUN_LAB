be030db6acf8159d1b3cddf5c7047a62aeadfd40
Antony Sánchez Herrera <77695208+antony02s@users.noreply.github.com>
2023-06-07 09:46:54 -0500

    reorganizing the repo

---
+++ WORKSHOP_1/Control_structures_and_loops/excercise_3/excercise_1/fibonacci.py
@@ -0,0 +1,7 @@
+# Write a Python program that prints the first 10 Fibonacci numbers using a loop.
+
+def fibonacci(loop_len):
+    a, b = 0, 1
+    for i in range(loop_len):
+        print(i)
+	    
